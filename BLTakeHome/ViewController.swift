2e0c428ecf3b7b2fa24696e9b52078e1dfa62d37
Matt Ao <mattyayoh@gmail.com>
2019-10-16 11:01:27 -0700

    example of how to subscribe to an interest AKA channel

--- BLTakeHome/ViewController.swift
+++ BLTakeHome/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PushNotifications
 
 class ViewController: UIViewController {
 
@@ -15,7 +16,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        try? PushNotifications.shared.addDeviceInterest(interest: "Global")
+        try? PushNotifications.shared.addDeviceInterest(interest: "Game_XYZ")
     }
 
     @IBAction func sendToUserToggled(_ sender: UISwitch) {
